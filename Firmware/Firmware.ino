7d1065ea1bacc5fd71df647b2fbe56dde175a1ea
Matan <matan@robo-plan.com>
2016-01-06 17:25:45 +0200

    first commit

---
+++ Firmware/Firmware.ino
@@ -0,0 +1,30 @@
+#include "Global.h"
+#define KnocksSequenceMaxLength 10
+
+int knockSequence[KnocksSequenceMaxLength];
+int tempKnockSequence[KnocksSequenceMaxLength];
+int knocksSequenceLength  = 0;
+
+void setup() 
+{
+	Serial.begin(9600);
+	
+	led.setRGB(0,0,255);
+	Serial.println("start");
+	
+}
+
+void loop() 
+{	
+  int sample = piezo.read();
+	if(sample > 10) //if sample above thershold
+	{
+			Serial.println("Piezo detect something, change LED");
+			led.setRGB(255,0,0);
+			delay(500);
+			led.setRGB(0,255,0);
+			delay(500);
+			led.setRGB(0,0,255);     
+	}
+ delay(100);
+}
